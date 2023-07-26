57f668f95e75eeb0ea9cd843e5bd69a4d6dae3e9
softwarecrash <lacrimosa2k2@gmail.com>
2023-07-26 22:58:10 +0200

    change bat watts

--- src/main.cpp
+++ src/main.cpp
@@ -692,9 +692,13 @@ bool getJsonData(int invNum)
   liveData["SOLAR_AMPS"] = live.l.pI / 100.f;
   liveData["SOLAR_WATTS"] = live.l.pP / 100.f;
   liveData["BATT_VOLTS"] = live.l.bV / 100.f;
+
   //liveData["BATT_AMPS"] = live.l.bI / 100.f;
   liveData["BATT_AMPS"] = batteryCurrent / 100.f;
-  liveData["BATT_WATTS"] = live.l.bP / 100.f;
+
+  //liveData["BATT_WATTS"] = live.l.bP / 100.f;
+  liveData["BATT_WATTS"] = (live.l.bV*batteryCurrent) / 100.f;
+
   liveData["LOAD_VOLTS"] = live.l.lV / 100.f;
   liveData["LOAD_AMPS"] = live.l.lI / 100.f;
   liveData["LOAD_WATTS"] = live.l.lP / 100.f;
@@ -817,9 +821,13 @@ bool sendtoMQTT(int invNum)
     mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/SOLAR_WATTS").c_str(), String(live.l.pP / 100.f).c_str());
     //Live Batt
     mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_VOLTS").c_str(), String(live.l.bV / 100.f).c_str());
+
     //mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_AMPS").c_str(), String(live.l.bI / 100.f).c_str());
     mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_AMPS").c_str(), String(batteryCurrent / 100.f).c_str());
-    mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_WATTS").c_str(), String(live.l.bP / 100.f).c_str());
+
+    //mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_WATTS").c_str(), String(live.l.bP / 100.f).c_str());
+    mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/BATT_WATTS").c_str(), String((live.l.bV*batteryCurrent) / 100.f).c_str());
+  
     //Live Load
     mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/LOAD_VOLTS").c_str(), String(live.l.lV / 100.f).c_str());
     mqttclient.publish((topic + "/" + mqttDeviceName + "/LiveData/LOAD_AMPS").c_str(), String(live.l.lI / 100.f).c_str());
