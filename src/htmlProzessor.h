dec84a93afc06b86329fe84b7eef3d209b6b41df
Softwarecrash <lacrimosa2k2@gmail.com>
2023-08-05 18:21:31 +0200

    correct html minify

--- src/htmlProzessor.h
+++ src/htmlProzessor.h
@@ -2,55 +2,31 @@
 String htmlProcessor(const String &var)
 {
     extern Settings _settings;
-    if (var == F("HEAD_TEMPLATE"))
+    if (var == F("pre_head_template"))
         return (HTML_HEAD);
-    if (var == F("FOOT_TEMPLATE"))
+    if (var == F("pre_foot_template"))
         return (HTML_FOOT);
-    if (var == F("SOFTWARE_VERSION"))
+    if (var == F("pre_software_version"))
         return (SOFTWARE_VERSION);
-    if (var == F("SWVERSION"))
+    if (var == F("pre_swversion"))
         return (SWVERSION);
-/*    if (var == F("Flash_Size"))
-        return (String(FlashSize).c_str());
-    if (var == F("ESP01"))
-        return (String(ESP01).c_str());
-    if (var == F("WAKEUP_PIN"))
-        return (String(WAKEUP_PIN).c_str());
-    if (var == F("RELAISPIN"))
-        return (String(RELAIS_PIN).c_str());
-*/    if (var == F("DEVICE_NAME"))
+   if (var == F("pre_device_name"))
         return (_settings._deviceName);
-    if (var == ("DEVICE_QUANTITY"))
+    if (var == ("pre_device_quantity"))
         return (String(_settings._deviceQuantity).c_str());
-    if (var == F("MQTT_SERVER"))
+    if (var == F("pre_mqtt_server"))
         return (_settings._mqttServer);
-    if (var == F("MQTT_PORT"))
+    if (var == F("pre_mqtt_port"))
         return (String(_settings._mqttPort).c_str());
-    if (var == F("MQTT_USER"))
+    if (var == F("pre_mqtt_user"))
         return (_settings._mqttUser);
-    if (var == F("MQTT_PASS"))
+    if (var == F("pre_mqtt_pass"))
         return (_settings._mqttPassword);
-    if (var == F("MQTT_TOPIC"))
+    if (var == F("pre_mqtt_topic"))
         return (_settings._mqttTopic);
-    if (var == F("MQTT_REFRESH"))
+    if (var == F("pre_mqtt_refresh"))
         return (String(_settings._mqttRefresh).c_str());
-    if (var == F("MQTT_JSON"))
+    if (var == F("pre_mqtt_json"))
         return (_settings._mqttJson ? "checked":"");
-/*    if (var == F("BMS_WAKE"))
-        return (_settings.data.wakeupEnable ? "checked":"");
-    if (var == F("RELAIS_ENABLE"))
-        return (_settings.data.relaisEnable ? "checked":"");
-    if (var == F("RELAIS_INVERT"))
-        return (_settings.data.relaisInvert ? "checked":"");
-    if (var == F("RELAIS_FAILSAVE"))
-        return (_settings.data.relaisFailsafe ? "checked":"");
-    if (var == F("RELAIS_VALUE"))
-        return (String(_settings.data.relaisSetValue, 3).c_str());
-    if (var == F("RELAIS_HYST"))
-        return (String(_settings.data.relaisHysteresis, 3).c_str());
-    if (var == F("RELAIS_FUNCTION"))
-        return (String(_settings.data.relaisFunction).c_str());
-    if (var == F("RELAIS_COMP"))
-        return (String(_settings.data.relaisComparsion).c_str());
-*/    return String();
+    return String();
 }
