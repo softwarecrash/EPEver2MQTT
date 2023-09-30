48c486fc65f473d41695a35d58a0c86d6f122c10
Softwarecrash <lacrimosa2k2@gmail.com>
2023-09-30 11:50:29 +0200

    optimize websocket

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,7 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
   {
   case WS_EVT_CONNECT:
     wsClient = client;
-    notifyClients();
+    //notifyClients();
     DEBUG_WEBF("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
     break;
   case WS_EVT_DISCONNECT:
