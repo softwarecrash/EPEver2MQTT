58ba7738ac20bc2bd8db9d783fcf765465604665
ManuA <17761850+all-solutions@users.noreply.github.com>
2024-10-18 12:17:38 +0200

    Reworked variables used
    Rework of the variables used to better identify the versions

--- tools/pre_compile.py
+++ tools/pre_compile.py
@@ -2,11 +2,11 @@ Import("env")
 
 env.Append(CPPDEFINES=[
     ("SWVERSION", env.StringifyMacro(env.GetProjectOption("custom_prog_version"))),
-    ("HWBOARD", env.StringifyMacro(env["BOARD"])),
+    ("HWBOARD", env.StringifyMacro(env["PIOENV"])),
 ])
 if env.GetProjectOption("build_type") == "debug":
     env.Append(CPPDEFINES=[
         ("isDEBUG",  env.StringifyMacro(env.GetBuildType())),
     ])
 
-env.Replace(PROGNAME="EPEver2MQTT_%s_%s" % (str(env["BOARD"]), env.GetProjectOption("custom_prog_version")))
+env.Replace(PROGNAME="EPEver2MQTT_%s_%s" % (str(env["PIOENV"]), env.GetProjectOption("custom_prog_version")))
